9a4a3061fa31c183f2eccbb41b018eb27cf8fd99
Valeriu Seremet <valeriuseremet97@gmail.com>
2021-01-30 13:52:01 +0200

    Add companyInfo Component and import apexcharts

--- Shareholders.Web/ShareholdersWeb/src/app/app.module.ts
+++ Shareholders.Web/ShareholdersWeb/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CompanyInfoComponent } from './company-info/company-info.component';
 import {NgApexchartsModule} from 'ng-apexcharts';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CompanyInfoComponent
   ],
   imports: [
     BrowserModule,
---
+++ Shareholders.Web/ShareholdersWeb/src/app/company-info/company-info.component.ts
@@ -0,0 +1,51 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexChart, ChartComponent
+} from 'ng-apexcharts';
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+};
+
+@Component({
+  selector: 'app-company-info',
+  templateUrl: './company-info.component.html',
+  styleUrls: ['./company-info.component.css']
+})
+export class CompanyInfoComponent implements OnInit {
+  public chartOptions: Partial<ChartOptions>;
+
+  @ViewChild('chart') chart: ChartComponent;
+  constructor() {
+    this.chartOptions = {
+      series: [44, 55, 13, 43, 22],
+      chart: {
+        width: 380,
+        type: 'pie'
+      },
+      labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: 'bottom'
+            }
+          }
+        }
+      ]
+    };
+  }
+
+  ngOnInit(): void {
+  }
+
+}
